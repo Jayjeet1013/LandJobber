61ed191926642d240c2eb6efa368f58eadd00876
Jayjeet1013 <jayjeet1013@gmail.com>
2024-03-10 00:19:00 +0530

    update

--- src/components/Data/Data.tsx
+++ src/components/Data/Data.tsx
@@ -11,5 +11,140 @@ export const Data = [
                 link:"https://wellfound.com/",
                 description:"job for remote and work from office"
         },
+        {
+                id:3,
+                companyName:"Talent Network",
+                link:"https://talentnetwork.io/",
+                description:"We help fast-growing companies recruit executives,managers and strategy consultants."
+        },
+        {
+                id:4,
+                companyName:"Turing",
+                link:"https://www.turing.com/",
+                description:"The world's first AI-powered tech services company"
+        },
+        {
+                id:5,
+                companyName:"Outscal",
+                link:"https://outscal.com/",
+                description:"Outscal is a community of game developers that helps people upskill and land jobs in the gaming industry.                "
+        },
+        {
+                id:6,
+                companyName:"spinks",
+                link:"https://www.wearespinks.com/",
+                description:"Connecting fast-growth organisations with exceptional talent."
+        },
+        {
+                id:7,
+                companyName:"Remotive",
+                link:"https://remotive.com/",
+                description:"Find your dream remote job without the hassle"
+        },
+        {
+                id:8,
+                companyName:"Foundit",
+                link:"https://www.foundit.in/",
+                description:"Over 5,00,000+ jobs to explore"
+        },
+        {
+                id:9,
+                companyName:"careers.shardeum.org",
+                link:"https://careers.shardeum.org/companies",
+                description:"Find Your Career"
+        },
+        {
+                id:10,
+                companyName:"Relevel",
+                link:"https://relevel.com/",
+                description:"Get your dream job in 10 days"
+        },
+        {
+                id:11,
+                companyName:"Toptal",
+                link:"https://www.toptal.com/",
+                description:"Hire the Top 3% of Freelance Talent®"
+        },
+        {
+                id:12,
+                companyName:"Talents4world",
+                link:"https://www.talents4world.com/",
+                description:"Jobs from all over the world"
+        },
+        {
+                id:13,
+                companyName:"Adzuna",
+                link:"https://www.adzuna.com/",
+                description:"Adzuna is the straight-shooting job search engine that helps you zero in on the right role"
+        },
+        {
+                id:14,
+                companyName:"Flexjobs",
+                link:"https://www.flexjobs.com/",
+                description:"Find your next flexible, hybrid, or work from home job."
+        },
+        {
+                id:15,
+                companyName:"LookingforJob",
+                link:"https://lookingforjob.co/",
+                description:"Find A Job Simple, fast and efficient"
+        },
+        {
+                id:16,
+                companyName:"Erekrut",
+                link:"https://www.erekrut.com/",
+                description:"Erekrut helps you create an effective customised recruitment process."
+        },
+
+        {
+                id:17,
+                companyName:"SonicJobs",
+                link:"https://www.sonicjobs.com/us",
+                description:"Apply in a few clicks.Get hired fast."
+        },
+        {
+                id:18,
+                companyName:"Cuvette",
+                link:"https://cuvette.tech/",
+                description:"Get quality Internships & Jobs in 3 steps"
+        },
+        {
+                id:19,
+                companyName:"Social3",
+                link:"https://app.social3.club/",
+                description:"Hire top web3 Talent Ship Fast."
+        },
+        {
+                id:20,
+                companyName:"Blockchainheadhunter",
+                link:"https://blockchainheadhunter.com/",
+                description:"Get quality Internships & Jobs in 3 steps"
+        },
+        {
+                id:21,
+                companyName:"Way2Fresher",
+                link:"https://www.way2fresher.com/",
+                description:"Find Your Career. You Deserve it ."
+        },
+        {
+                id:22,
+                companyName:"Generationshe",
+                link:"https://www.generationshe.co/",
+                description:"             You’ve got dreams.           We’ve got direction."
+        },
+        {
+                id:23,
+                companyName:"Hashtagweb3",
+                link:"https://www.hashtagweb3.org/",
+                description:"Get quality Internships & Jobs in 3 steps"
+        },
+        {
+                id:24,
+                companyName:"Internship Corners",
+                link:"https://internshipcorners.com/",
+                description:"We Offer A Cornucopia Of Opportunities From Various Organizations And Companies Around The World."
+        },
+
+
 ]
 
