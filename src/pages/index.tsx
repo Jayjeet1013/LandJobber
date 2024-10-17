bff7008f2d73628687e5d4816f745830cdba37f5
Jayjeet1013 <jayjeet1013@gmail.com>
2024-10-17 21:56:49 +0530

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,6 @@ import { useEffect } from "react";
 import ReactGA from "react-ga";
 
 export default function Home() {
-  useEffect(() => {
-    ReactGA.initialize("G-YRW3H22G57");
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
   return (
     <div>
       <Head>
@@ -47,24 +42,6 @@ export default function Home() {
         <meta name="author" content="LandJobber Team" />
         <link rel="canonical" href="https://landjobber.vercel.app/" />
         <link rel="icon" href="/logo.avif" />
-
-        {/* Google Tag Manager for Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
 
         {/* Structured Data (JSON-LD) for SEO */}
         <script
