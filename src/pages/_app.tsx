98c8374f8b89f0a56af8c3d6640fe9afcf19d13b
Jayjeet1013 <jayjeet1013@gmail.com>
2024-10-17 21:45:53 +0530

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import Nav from "@/components/layout/Nav";
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <ChakraProvider>
         <Nav />
         <Component {...pageProps} />
+        <Analytics />
         <Footer />
       </ChakraProvider>
     </>
